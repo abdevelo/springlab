908072f3db4eb3cf3cebfd54d386ff228a268810
abdevelo <talove0531@gmail.com>
2023-04-20 16:04:13 +0900

    0420

---
+++ src/main/java/com/kbstar/dto/CustDTO.java
@@ -0,0 +1,14 @@
+package com.kbstar.dto;
+
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@ToString
+public class CustDTO {
+    private String id;
+    private String pwd;
+    private String name;
+}
