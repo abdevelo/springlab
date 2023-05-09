1b9825eeb10fa467870fa63254aaacdd8170d695
hyosunpark <talove0531@gmail.com>
2023-05-09 09:02:47 +0900

    0509

--- src/main/java/com/kbstar/Web14Application.java
+++ src/main/java/com/kbstar/Web14Application.java
@@ -5,10 +5,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @SpringBootApplication
-public class Web13Application {
+public class Web14Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(Web13Application.class, args);
+        SpringApplication.run(Web14Application.class, args);
     }
     @Bean
     public InternalResourceViewResolver setupViewResolver() {
